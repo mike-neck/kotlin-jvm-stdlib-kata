e34b47f22667e45ac39c2abde26b0c54a45de221
mike_neck <jkrt3333@gmail.com>
2020-12-20 18:27:10 +0900

    add: first exercise

---
+++ pet-kata/src/test/kotlin/com/example/pets/Exercise1.kt
@@ -0,0 +1,69 @@
+package com.example.pets
+
+import com.example.pets.fixtures.People
+import com.example.pets.fixtures.PetDomain
+import com.example.pets.fixtures.assertAll
+import io.kotest.matchers.collections.shouldHaveAtLeastSize
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(PetDomain::class)
+class Exercise1 {
+
+  // This method can be deleted
+  @BeforeEach
+  fun notNullIterable(people: People) {
+    assertAll { 
+      test { people shouldNotBe null }
+      test { people.items shouldHaveAtLeastSize 1 }
+    }
+  }
+
+  @Test
+  fun firstNamesOfAllPeople(people: People) {
+    // Replace a block { mutableListOf() }, with a transformation method on MutableList(people.items)
+    val firstNames: MutableList<String> = people { mutableListOf() } // people.items...
+
+    val expectedFirstNames: List<String> = listOf("Mary", "Bob", "Ted", "Jake", "Barry", "Terry", "Harry", "John")
+
+    firstNames shouldBe expectedFirstNames
+  }
+
+  @Test
+  fun namesOfMarySmithsPets(people: People) {
+    // Find person having name "Mary Smith"
+    val mayPerson: Person? = people<Person?> {
+      // You can know a person has name "May Smith" via named(String) function
+      // val boolean: Boolean = person.named("Mary Smith")
+      Person("", "", mutableListOf())
+    }
+    val person: Person = mayPerson ?: throw IllegalStateException("$mayPerson should be not null")
+
+    val pets: List<Pet> = person.pets
+
+    // Replace a block { mutableListOf() }, with a transformation method on MutableList()
+    val names: List<String> = pets { mutableListOf() }
+
+    names.joinToString() shouldBe "Tabby"
+  }
+
+  @Test
+  fun peopleWithCats(people: People) {
+    // Replace a block { mutableListOf() }, with a positive filtering method on MutableList()
+    val peopleWithCats: MutableList<Person> = people { mutableListOf() }
+
+    peopleWithCats shouldHaveSize 2
+  }
+
+  @Test
+  fun peopleWithoutCats(people: People) {
+    // Replace a block { mutableListOf() }, with a negative filtering method on MutableList()
+    val peopleWithCats: MutableList<Person> = people { mutableListOf() }
+
+    peopleWithCats shouldHaveSize 6
+  }
+}
