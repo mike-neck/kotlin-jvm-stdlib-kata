cd34ff6b8dbdeb46fcc1bb84dc3cea26ce28ac9b
mike_neck <jkrt3333@gmail.com>
2020-12-20 18:26:07 +0900

    add: pet model

---
+++ pet-model/src/main/kotlin/com/example/pets/pet-pet-type-person.kt
@@ -0,0 +1,47 @@
+package com.example.pets
+
+import data.Bag
+import org.jetbrains.annotations.TestOnly
+
+operator fun <T: Any, R> Iterable<T>.invoke(imitation: () -> R): R = imitation()
+
+enum class PetType {
+  CAT,
+  DOG,
+  HAMSTER,
+  TURTLE,
+  BIRD,
+  SNAKE,
+  ;
+}
+
+data class Pet(
+  val type: PetType,
+  val name: String,
+  val age: Int
+)
+
+data class Person(
+  val firstName: String,
+  val lastName: String,
+  private val ownsPets: MutableList<Pet>
+) {
+  @TestOnly
+  operator fun <T> invoke(imitation: () -> T): T = imitation()
+
+  val pets: List<Pet> get() = listOf(*ownsPets.toTypedArray())
+
+  val petTypes: Bag<PetType> get() = pets.groupBy { it.type }.mapValues { it.value.size }
+
+  val numberOfPets: Int get() = pets.size
+
+  val isPetPerson: Boolean get() = pets.isNotEmpty()
+
+  fun named(name: String): Boolean = name == "$firstName $lastName"
+
+  fun hasPet(type: PetType): Boolean = pets.any { it.type == type }
+
+  fun addPet(type: PetType, name: String, age: Int) = addPet(Pet(type, name, age))
+
+  fun addPet(pet: Pet): Unit = ownsPets.add(pet).let { }
+}
