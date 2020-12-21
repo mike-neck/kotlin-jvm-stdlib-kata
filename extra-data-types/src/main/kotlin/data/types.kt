f6f240eb01bcb0761c60bfe5c39c8a922402bba1
mike_neck <jkrt3333@gmail.com>
2020-12-21 18:53:32 +0900

    add: Exercise3 test 2 Multimap

--- extra-data-types/src/main/kotlin/data/types.kt
+++ extra-data-types/src/main/kotlin/data/types.kt
@@ -12,3 +12,7 @@ fun <T : Any> MutableBag<T>.add(t: T): Unit =
 
 operator fun <T : Any> MutableBag<T>.plusAssign(t: T): Unit =
     this.compute(t) { _, count -> if (count == null) 1 else count + 1 }.let {}
+
+typealias Multimap<K, V> = Map<K, List<V>>
+
+typealias MutableMultimap<K, V> = MutableMap<K, V>
