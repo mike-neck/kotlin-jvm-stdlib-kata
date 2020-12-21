b4f3f3775c605867235b38dc5046463539fe608e
mike_neck <jkrt3333@gmail.com>
2020-12-21 18:15:54 +0900

    add: Exercise3 test 1 main test.

--- pet-kata/src/test/kotlin/com/example/pets/Exercise3.kt
+++ pet-kata/src/test/kotlin/com/example/pets/Exercise3.kt
@@ -2,6 +2,7 @@ package com.example.pets
 
 import com.example.pets.fixtures.PetDomain
 import com.example.pets.fixtures.dynamic
+import data.Bag
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
@@ -22,8 +23,19 @@ class Exercise3 {
         test("The count of cat is 2") { petTypeCounts[PetType.CAT] shouldBe 2 }
         test("The count of dog is 2") { petTypeCounts[PetType.DOG] shouldBe 2 }
         test("The count of hamster is 2") { petTypeCounts[PetType.HAMSTER] shouldBe 2 }
-        test("The count of snake is 2") { petTypeCounts[PetType.SNAKE] shouldBe 1 }
-        test("The count of turtle is 2") { petTypeCounts[PetType.TURTLE] shouldBe 1 }
-        test("The count of bird is 2") { petTypeCounts[PetType.BIRD] shouldBe 1 }
+        test("The count of snake is 1") { petTypeCounts[PetType.SNAKE] shouldBe 1 }
+        test("The count of turtle is 1") { petTypeCounts[PetType.TURTLE] shouldBe 1 }
+        test("The count of bird is 1") { petTypeCounts[PetType.BIRD] shouldBe 1 }
+
+        // Hint: use the appropriate method on `people` to create a Bag<PetType>
+        // Hint: Bag<PetType> is alias for Map<PetType, Int> in this tutorial
+        val counts: Bag<PetType> = people { mutableMapOf() }
+
+        test("The count of cat is 2") { counts[PetType.CAT] shouldBe 2 }
+        test("The count of dog is 2") { counts[PetType.DOG] shouldBe 2 }
+        test("The count of hamster is 2") { counts[PetType.HAMSTER] shouldBe 2 }
+        test("The count of snake is 1") { counts[PetType.SNAKE] shouldBe 1 }
+        test("The count of turtle is 1") { counts[PetType.TURTLE] shouldBe 1 }
+        test("The count of bird is 1") { counts[PetType.BIRD] shouldBe 1 }
       }
 }
