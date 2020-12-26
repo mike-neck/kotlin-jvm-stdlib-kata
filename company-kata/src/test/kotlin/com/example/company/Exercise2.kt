d471151cbd11959e0c44f547cde2f019493d944a
mike_neck <jkrt3333@gmail.com>
2020-12-26 16:05:37 +0900

    add: Exercise 2 test 2-7

--- company-kata/src/test/kotlin/com/example/company/Exercise2.kt
+++ company-kata/src/test/kotlin/com/example/company/Exercise2.kt
@@ -1,9 +1,19 @@
 package com.example.company
 
+import com.example.company.CompanyDomain.Companion.invoke
+import data.PartitionList
+import data.emptyPartitionList
+import data.rejected
+import data.selected
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.ints.shouldBeExactly
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.DynamicTest
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(CompanyDomain::class)
 class Exercise2 {
 
   @TestFactory
@@ -19,6 +29,54 @@ class Exercise2 {
         },
         DynamicTest.dynamicTest("customer from NY comes not from London") {
           predicateThatTestsCustomerFromLondon(customerFromNewYork) shouldBe false
+        })
+  }
+
+  @Test
+  fun doAnyCustomerLiveInLondon(company: Company) {
+    val anyCustomerFromLondon: Boolean = company { false }
+
+    anyCustomerFromLondon shouldBe true
+  }
+
+  @Test
+  fun doAllCustomersLiveInLondon(company: Company) {
+    val allCustomerFromLondon: Boolean = company { false }
+
+    allCustomerFromLondon shouldBe true
+  }
+
+  @Test
+  fun howManyCustomersLiveInLondon(company: Company) {
+    val numberOfCustomerFromLondon: Int = company { 1000 }
+
+    numberOfCustomerFromLondon shouldBeExactly 2
+  }
+
+  @Test
+  fun customersWhoLiveInLondon(company: Company) {
+    val customersFromLondon: List<Customer> = company { emptyList() }
+
+    customersFromLondon shouldHaveSize 2
+  }
+
+  @Test
+  fun customersWhoDontLiveInLondon(company: Company) {
+    val customersNotFromLondon: List<Customer> = company { emptyList() }
+
+    customersNotFromLondon shouldHaveSize 1
+  }
+
+  @TestFactory
+  fun customersPartitionedByWhetherLivingInLondon(company: Company): Iterable<DynamicTest> {
+    val partitionList: PartitionList<Customer> = company { emptyPartitionList() }
+
+    return listOf(
+        DynamicTest.dynamicTest("2 customers live in London") {
+          partitionList.selected shouldHaveSize 2
+        },
+        DynamicTest.dynamicTest("A customer does not live in London") {
+          partitionList.rejected shouldHaveSize 1
         })
   }
 }
