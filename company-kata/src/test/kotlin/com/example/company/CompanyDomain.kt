92136d7067d371393b60ed3967077f0d06ac55a2
mike_neck <jkrt3333@gmail.com>
2020-12-27 13:26:52 +0900

    add: copied dynamic test utilities

--- company-kata/src/test/kotlin/com/example/company/CompanyDomain.kt
+++ company-kata/src/test/kotlin/com/example/company/CompanyDomain.kt
@@ -1,8 +1,10 @@
 package com.example.company
 
+import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.api.extension.ParameterContext
 import org.junit.jupiter.api.extension.ParameterResolver
+import org.junit.jupiter.api.fail
 
 class CompanyDomain : ParameterResolver {
 
@@ -160,3 +162,25 @@ private class SupplierBuilder(
           name = this.name ?: throw IllegalArgumentException("name"),
           itemNames = this.itemNames.toTypedArray())
 }
+
+class DynamicTestItem(val title: String, val executable: () -> Unit)
+
+class DynamicTestFactory(
+    private val items: MutableList<DynamicTestItem> = mutableListOf(),
+    private val beforeCallbacks: MutableList<(String) -> Unit> = mutableListOf()
+) : Iterable<DynamicTestItem> by items {
+  fun test(title: String, executable: () -> Unit): Unit =
+      items.add(
+              DynamicTestItem(title) {
+                beforeCallbacks.forEach { it(title) }
+                executable()
+              })
+          .let {}
+
+  fun failAll(memo: String): Unit = beforeCallbacks.add { fail("$memo[$it]") }.let {}
+}
+
+fun dynamic(configure: DynamicTestFactory.() -> Unit): Iterable<DynamicTest> =
+    DynamicTestFactory().also(configure).map {
+      DynamicTest.dynamicTest(it.title) { it.executable() }
+    }
